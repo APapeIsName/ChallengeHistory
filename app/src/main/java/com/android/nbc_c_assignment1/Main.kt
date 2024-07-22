194a60242ef530ad6bc09b3bd3e77ed4619f5260
APapeIsName <gms72901217@gmail.com>
2024-07-22 09:52:34 +0900

    test

--- app/src/main/java/com/android/nbc_c_assignment1/Main.kt
+++ app/src/main/java/com/android/nbc_c_assignment1/Main.kt
@@ -1,11 +1,5 @@
 package com.android.nbc_c_assignment1
 
-import com.android.nbc_c_assignment1.challenge2.challenge2First
-import com.android.nbc_c_assignment1.challenge2.challenge2Second
-
 fun main() {
-//    challenge1()
-//    challenge2First()
-//    challenge2Second()
 
 }
